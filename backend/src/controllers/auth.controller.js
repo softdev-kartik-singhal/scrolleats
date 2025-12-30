b628d1c1e82e396d39fade33f0e00f71a64c320e
Kartik Singhal <kartiksinghal2803@gmail.com>
2025-12-30 20:35:19 +0530

    Fix: Update loginFoodPartner cookie settings

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -164,9 +164,9 @@ async function loginFoodPartner(req, res) {
     }, process.env.JWT_SECRET)
     res.cookie("token", token, {
         httpOnly: true,
-        secure: false,        // true in production (https)
-        sameSite: "lax",      // IMPORTANT
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        secure: true,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
 
